3fe1cf91c49e13d7b9638c4c5bafe1bb567e7fdd
SenhorContritus <eduardo.martinez@unesp.br>
2025-03-22 09:01:28 -0300

    não cabei

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -1,43 +1,39 @@
-<html>
-    <head>
-        <style>
-            td{
-                width: 100px;
-            }
-            th{
-                width: 100px;
-            }
-        </style>
-    </head>
-    <body>
-        <h3>Visão Pog</h3>
-        <div>
-            <table border="1" >
-                <tr>
-                    <th>
-                        Id
-                    </th>
-                    <th>
-                        Nome
-                    </th>
-                    <th>
-                        Telefone
-                    </th>
-                </tr>               
-                @foreach ($contatos as $contato)<!-- for each que mostra cada linha da matriz contatos utilizando a variavel contato-->
-                <tr>
-                    <td>
-                        {{ $contato['id'] }}<!--mostra o valor desejado com base no nome declarado na matriz-->
-                    </td>
-                    <td>
-                        {{ $contato['nome'] }}
-                    </td>
-                    <td>
-                        {{ $contato['cel'] }}
-                    </td>
-                </tr>
-                @endforeach           
-            </table>
-        </div>
-    </body>
-</html>
+@extends("layouts.main")
+
+@section('title', "view pogger")
+
+
+@section("content")
+    <div>
+        <table border="1" >
+            <tr>
+                <th>
+                    Id
+                </th>
+                <th>
+                    Nome
+                </th>
+                <th>
+                    Telefone
+                </th>
+            </tr>               
+            @foreach ($contatos as $contato)<!-- for each que mostra cada linha da matriz contatos utilizando a variavel contato-->
+            <tr>
+                <td>
+                    {{ $contato['id'] }}<!--mostra o valor desejado com base no nome declarado na matriz-->
+                </td>
+                <td>
+                    {{ $contato['nome'] }}
+                </td>
+                <td>
+                    {{ $contato['cel'] }}
+                </td>
+            </tr>
+            @endforeach           
+        </table>
+    </div>
+
+@endsection
+
+
+
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <style>
+            td{
+                width: 100px;
+            }
+            th{
+                width: 100px;
+            }
+        </style>
+    </head>
+    <body>
+    <title>@yield("title")</title>
